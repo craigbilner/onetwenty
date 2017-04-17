08e891b1fccae3e78e4cb3a46191f273abe98a1e
Craig Bilner <craig.bilner@ig.com>
2017-04-17 17:26:48 +0100

    Add css versioning and critical inlining

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const rev = require('gulp-rev');
+const cssc = require('gulp-css-condense');
+const clean = require('gulp-clean');
+const fc2json = require('gulp-file-contents-to-json');
+const concatCss = require('gulp-concat-css');
+
+const THEME_DIR = 'themes/onetwenty/';
+const CSS_DIR = `${THEME_DIR}static/css/`;
+const OUT_DIR = `${CSS_DIR}/dist`;
+const SKELETON_DIR = `${CSS_DIR}/skeletons/`;
+
+gulp.task('clean', () =>
+  gulp.src(OUT_DIR, { read: false })
+    .pipe(clean())
+);
+
+gulp.task('minify-css', ['clean'], () =>
+  gulp.src(`${CSS_DIR}*.css`)
+    .pipe(concatCss('bundle.css'))
+    .pipe(cssc())
+    .pipe(rev())
+    .pipe(gulp.dest(OUT_DIR))
+    .pipe(rev.manifest('manifestcss.json'))
+    .pipe(gulp.dest(`${THEME_DIR}/data`))
+);
+
+gulp.task('make-skeletons', () =>
+  gulp.src(`${SKELETON_DIR}*.css`)
+    .pipe(cssc())
+    .pipe(fc2json('skeletons.json', {
+      extname: false,
+    }))
+    .pipe(gulp.dest(`${THEME_DIR}/data`))
+);
+
+gulp.task('build-css', ['minify-css', 'make-skeletons']);
